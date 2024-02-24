daff9ec0042623aaffbbb1e1c39c6cb6451cf150
Guy-Ben-Yosef <guyben@campus.technion.ac.il>
2024-02-24 17:46:54 +0200

    All basic calculation are done, need to be verified and validate with the new data

--- main.m
+++ main.m
@@ -8,7 +8,8 @@ Data2 = load('Data/shir.mat').AngularVelocity;
 time1  = seconds(Data1.Timestamp - Data1.Timestamp(1));
 Omega1 = [Data1.X, Data1.Y, Data1.Z];
 
-time2  = seconds(Data2.Timestamp - Data2.Timestamp(1));
+dt = 0.04;
+time2  = seconds(Data2.Timestamp - Data2.Timestamp(1)) + dt;
 Omega2 = [Data2.X, Data2.Y, Data2.Z];
 
 clear Data1 Data2
@@ -24,8 +25,45 @@ bias2 = mean( Omega2( (time2 >= bias_lrn_strt) & ...
 rlvnt_strt = 33.2;
 rlvnt_stop = 51;
 
-Omega1 = Omega1( (time1 >= rlvnt_strt) & (time1 <= rlvnt_stop) ) - bias1;
-Omega2 = Omega2( (time2 >= rlvnt_strt) & (time2 <= rlvnt_stop) ) - bias2;
+rlvnt_mask1 = (time1 >= rlvnt_strt) & (time1 <= rlvnt_stop);
+rlvnt_mask2 = (time2 >= rlvnt_strt) & (time2 <= rlvnt_stop);
 
+time1 = time1( rlvnt_mask1 );
+time1 = time1 - time1(1);
+time2 = time2( rlvnt_mask2 );
+time2 = time2 - time2(1);
 
+Omega1 = Omega1( rlvnt_mask1, : ) - bias1;
+Omega2 = Omega2( rlvnt_mask2, : ) - bias2;
 
+[mutual_time, interp_Omega1, interp_Omega2] = interpolateOmega(time1, Omega1, time2, Omega2);
+C = calculateRelativeScaleFactor(interp_Omega1, interp_Omega2);
+calib_Omega1 = interp_Omega1 * C;
+T = calculateRotationMatrix(calib_Omega1, interp_Omega1);
+rot_Omega1 = calib_Omega1 * T;
+
+%%
+figure
+subplot(3,1,1)
+title('$x$', 'Interpreter', 'latex')
+hold on
+plot(mutual_time, rot_Omega1(:, 1), 'LineWidth', 1.5)
+plot(mutual_time, interp_Omega2(:, 1), 'LineWidth', 1.5)
+
+grid on
+
+subplot(3,1,2)
+title('$y$', 'Interpreter', 'latex')
+hold on
+plot(mutual_time, rot_Omega1(:, 2), 'LineWidth', 1.5)
+plot(mutual_time, interp_Omega2(:, 2), 'LineWidth', 1.5)
+grid on
+
+subplot(3,1,3)
+title('$z$', 'Interpreter', 'latex')
+hold on
+plot(mutual_time, rot_Omega1(:, 3), 'LineWidth', 1.5, 'DisplayNam', 'Rotated $\Omega_{1}$')
+plot(mutual_time, interp_Omega2(:, 3), 'LineWidth', 1.5, 'DisplayNam', '$\Omega_{2}$')
+grid on
+
+legend('Interpreter', 'latex')
